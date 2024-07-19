2c1ba146b1911b36dc3cf9d09cc22ed89416a2d0
Arttu Puttonen <arskaputtonen@gmail.com>
2024-07-19 11:13:26 +0300

    9.11 korjattu

--- Patientor/backend/src/index.ts
+++ Patientor/backend/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import diagnosesRouter from './routes/diagnoses';
 import patientsRouter from './routes/patients';
-
+import cors from 'cors';
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
